857b50eb1078c3e13b94384d44a26321bec48ca8
to4my <to4my@ya.ru>
2019-12-03 14:53:26 +0300

    SQL error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,55 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Mikhail',
+        username: 'to4my',
+        avatar_url: 'https://lh3.googleusercontent.com/-y12HGFkzEgc/AAAAAAAAAAI/AAAAAAAAAAA/' \
+        'ACHi3rd1dpiwuhmobBbYy8kvzKdPNVTQ4g.CMID/s83-c/photo.jpg',
+        email: 'to4my@ya.ru',
+        password: '12345'
+
+      ),
+      User.new(
+        id: 2,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100',
+        email: 'installero@ya.ru',
+        password: '12345'
+      ),
+      User.new(id: 3,
+               name: 'Misha',
+               username: 'aristofun',
+               email: 'aristofun@ya.ru',
+               password: '12345')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @time = Time.now
-    @hello = "Привет!"
+    @user = User.new(
+      name: 'Mikhail',
+      username: 'to4my',
+      avatar_url: 'https://lh3.googleusercontent.com/-y12HGFkzEgc/AAAAAAAAAAI/AAAAAAAAAAA/' \
+        'ACHi3rd1dpiwuhmobBbYy8kvzKdPNVTQ4g.CMID/s83-c/photo.jpg',
+      email: 'to4my@ya.ru',
+      password: '12345'
+
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('02.12.2019')),
+      Question.new(text: 'How much is the fish?', created_at: Date.parse('01.12.2019'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+$(function(){
+    $('#ask-button').click(function(){
+        $('#ask-form').slideToggle(300);
+        return false;
+    });
+});
