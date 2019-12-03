8c7b8b64edf03748e70f2bb311d50d677a98f973
to4my <to4my@ya.ru>
2019-12-03 13:15:36 +0300

    Change model Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
-class Question < ApplicationRecord
+class Question < ActiveRecord::Base
+  belongs_to :user
+
+  validates :user, :text, presence: true
+  validates :text, length: { maximum: 255 }
+
 end
